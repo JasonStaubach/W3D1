57de56c4df918d5313544aedc603200eddc287a2
JasonStaubach <jbstaubach@gmail.com>
2023-05-15 16:17:55 -0400

    added myzip

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -1,9 +1,9 @@
-
+require "byebug"
 
 
 class Array 
 
-    def my_each(&block)
+    def times(&block)
 
         length = self.length 
         i = 0 
@@ -16,7 +16,7 @@ class Array
     
     def my_select(&prc)
         select_arr = []
-        self.my_each() do |i|
+        self.times() do |i|
             select_arr << i if prc.call(i)
         end
         select_arr
@@ -25,14 +25,14 @@ class Array
 
     def my_reject(&prc)
         select_arr = []
-        self.my_each() do |i|
+        self.times() do |i|
             select_arr << i if !prc.call(i)
         end
         select_arr
     end
 
     def my_any?(&prc)
-        self.my_each() do |i|
+        self.times() do |i|
             return true if prc.call(i)
         end
     return false
@@ -40,7 +40,7 @@ class Array
 
 
     def my_all?(&prc)
-        self.my_each() do |i|
+        self.times() do |i|
             return false if !prc.call(i)
         end
     return true
@@ -57,10 +57,14 @@ class Array
 
     def my_zip(*args)
         res = []
-        (0...self.length).my_each do |i|
-            args.my_each do |j|
-                res[i] << args[i][j]
-            end 
+        self.length.times do |i|
+            miniArr = []
+            
+            miniArr << self[i]
+            (args[0].length - 1).times do |j|
+                miniArr << args[j][i]
+            end
+            res << miniArr 
 
         end
         res 
@@ -68,9 +72,9 @@ class Array
 
     a = [ 4, 5, 6 ]
     b = [ 7, 8, 9 ]
-    [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-    a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-    [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+    p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+    p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
     
     c = [10, 11, 12]
     d = [13, 14, 15]
